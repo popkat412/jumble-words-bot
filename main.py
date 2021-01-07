fdd40967321ff1eebea547e2a006963adca1f3d4
wangy <wangyunze412@gmail.com>
2021-01-07 14:40:37 +0800

    Add logging features

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import discord
+import logging
 from dotenv import dotenv_values
 from jumble_words import jumble_words
 
 COMMAND_PREFIX = "$"
+
 config = dotenv_values(".env")
+logging.basicConfig(
+    filename="jumble-words-bot.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(name)s: %(message)s",
+)
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    logging.info(f"Logged in as {client.user}")
 
 
 @client.event
